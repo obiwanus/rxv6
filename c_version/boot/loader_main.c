9a81b4514a6cd204f2377f90b52e53d0d8bc0cc1
Ivan Ivanov <ivan@ivanovs.info>
2020-03-26 23:41:44 +0100

    trying inline assembly

--- c_version/boot/loader_main.c
+++ c_version/boot/loader_main.c
@@ -1,4 +1,19 @@
 
-void bootmain() {
+static int addone(int src) {
+  int dst;
 
+  asm volatile(
+      "mov %1, %0\n\t"
+      "add 1, %0"
+      : "=r"(dst)
+      : "r"(src));
+
+  return dst;
+}
+
+int bootmain() {
+  int a = 1;
+  int b = addone(a);
+
+  return b;
 }
