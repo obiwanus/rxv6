7bb310ae571649acb3251881878206144bef1d9e
Ivan Ivanov <ivan@ivanovs.info>
2020-04-06 20:37:26 +0200

    function to output u8

--- c_version/include/x86_asm.h
+++ c_version/include/x86_asm.h
@@ -2,7 +2,7 @@
 
 static inline u8 in_u8(u16 port) {
   u16 data;
-  asm volatile("in %1, %0" : "=a"(data) : "d"(port));
+  asm volatile("in %[port], %[data]" : [data] "=a"(data) : [port] "d"(port));
   return data;
 }
 
@@ -14,6 +14,5 @@ static inline void in_u32_array(u16 port, void *addr, int count) {
 }
 
 static inline void out_u8(u16 port, u8 data) {
-  // TODO: write this function myself
-  // - understand extended asm
+  asm volatile("out %[data], %[port]" : : [data] "a"(data), [port] "d"(port));
 }
